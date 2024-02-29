798523c630b3d4762dac5800fc88dcce72ec7523
amiin <najjar.amiin@gmail.com>
2024-02-29 22:57:57 +0100

    - WorkoutDataTable.vue cleaned up - moved Methods to workoutOverview.vue

--- components/WorkoutDataTable.vue
+++ components/WorkoutDataTable.vue
@@ -2,171 +2,41 @@
 import {ref} from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import {deleteWorkoutData, fetchAndLogAllData, fetchWorkoutData} from "~/firebase/DBController";
 import {type WorkoutData} from "~/models/formData/workoutData"
-import Toolbar from "primevue/toolbar";
 
 
-const workouts = ref<WorkoutData[]>([]);
 const currentWorkoutSelection = ref<WorkoutData | null>(null);
+const emits = defineEmits(['updateSelection']);
 
 const props = defineProps({
-  workouts: Object,
-  currentWorkoutSelection: Object
+  workouts: Array
 });
 
-function formatDate(date: Date): string {
-  const day = ('0' + date.getDate()).slice(-2);
-  const month = ('0' + (date.getMonth() + 1)).slice(-2);
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-}
-
-
-// TODO: Format the data to dd/mm/yyyy
-// TODO: Add Remove and modify functionality to DataTable
-onBeforeMount(async () => {
-  const data = await fetchWorkoutData();
-  //console.log('data => ', data)
-  //@ts-ignore
-  workouts.value = data.map((workout, index) => ({
-    ...workout,
-    id: workout.id,
-    nr: index + 1,
-    generalInformation: {
-      ...workout.generalInformation,
-      //@ts-ignore
-      date: formatDate(workout.generalInformation.date)
-    }
-
-  }));
-  // For Debugging purposes
-  console.log(data);
+watch(currentWorkoutSelection, (newSelection) => {
+  emits('updateSelection', newSelection);
 });
-
-onMounted(() => {
-  fetchAndLogAllData();
-});
-
-const isEditMode = ref(false);
-const dataEntryFormVisible = ref(false); // Controls the visibility of DataEntryForm
-function toggleDataEntryForm() {
-  isEditMode.value = false;
-  dataEntryFormVisible.value = !dataEntryFormVisible.value;
-}
-
-//refreshes the DataTAble when new data is added or removed
-async function refreshData() {
-  const data = await fetchWorkoutData();
-  //@ts-ignore
-  workouts.value = data.map((workout, index) => ({
-    ...workout,
-    id: workout.id,
-    nr: index + 1,
-    generalInformation: {
-      ...workout.generalInformation,
-      //@ts-ignore
-      date: formatDate(workout.generalInformation.date)
-    }
-  }));
-}
-
-async function deleteSelectedWorkout() {
-  if (currentWorkoutSelection.value) {
-    try {
-      //@ts-ignore
-      await deleteWorkoutData(currentWorkoutSelection.value.id);
-      await refreshData();
-      currentWorkoutSelection.value = null;
-      console.log("Workout successfully deleted");
-    } catch (error) {
-      console.error("Failed to delete workout:", error);
-    }
-  } else {
-    console.log("No workout selected for deletion");
-  }
-}
-
-async function openFormInEditMode() {
-  if (currentWorkoutSelection.value) {
-    try {
-      dataEntryFormVisible.value = true;
-      isEditMode.value = true;
-
-      //await updateWorkoutData(currentWorkoutSelection.value.id);
-      //await refreshData();
-      // currentWorkoutSelection.value = null;
-      console.log("Workout successfully edited");
-    } catch (error) {
-      console.error("Failed to edit workout:", error);
-    }
-  } else {
-    console.log("No workout selected for deletion")
-  }
-}
-
-watch(currentWorkoutSelection, (newValue) => {
-  console.log(JSON.stringify(newValue))
-})
 
 </script>
 
 <template>
-
-  <DataEntryForm v-if="!isEditMode" v-model:visible="dataEntryFormVisible" :isEditMode="false"
-                 @dataSaved="refreshData" :prefilledWorkoutData="currentWorkoutSelection"/>
-  <DataEntryForm v-else v-model:visible="dataEntryFormVisible" :isEditMode="true"
-                 @dataSaved="refreshData" :prefilledWorkoutData="currentWorkoutSelection"/>
-
-
-  <div class="table-toolbar-container">
-    <div class="toolbar-container">
-      <Toolbar>
-        <template #start>
-          <Button class="toolbar-button" label="New" icon="pi pi-plus" @click="toggleDataEntryForm"/>
-          <Button class="toolbar-button" label="Edit" icon="pi pi-file-edit" severity="secondary"
-                  @click="openFormInEditMode"/>
-          <Button class="toolbar-button" label="Delete" icon="pi pi-trash" severity="danger"
-                  @click="deleteSelectedWorkout"/>
-          <Button class="toolbar-button" label="Refresh Table" icon="pi pi-undo" severity="info" @click="refreshData"/>
-        </template>
-      </Toolbar>
-    </div>
-    <DataTable v-model:selection="currentWorkoutSelection" dataKey="nr" :value="workouts" showGridlines
-               class="custom-table-Design" resizableColumns columnResizeMode="fit">
-      <Column selectionMode="single" headerStyle="width: 3rem"></Column>
-      <Column field="nr" header="Nr" style="width: 10%"></Column>
-      <Column field="generalInformation.date" header="Date" sortable style="width: 30%"/>
-      <Column field="generalInformation.name" header="Name" sortable style="width: 30%"/>
-      <Column field="suppIntakeInfo.bcaa" header="BCAA" style="width: 5%"/>
-      <Column field="suppIntakeInfo.bcaaAmount" header="gr" style="width: 2%"/>
-      <Column field="suppIntakeInfo.creatin" header="Creatin" style="width: 5%"/>
-      <Column field="suppIntakeInfo.creatinAmount" header="gr" style="width: 2%"/>
-      <Column field="suppIntakeInfo.eaa" header="EAA" style="width: 5%"/>
-      <Column field="suppIntakeInfo.eaaAmount" header="gr" style="width: 2%"/>
-      <Column field="suppIntakeInfo.protein" header="Protein" style="width: 5%"/>
-      <Column field="suppIntakeInfo.proteinAmount" header="gr" style="width: 2%"/>
-    </DataTable>
-  </div>
+  <DataTable v-model:selection="currentWorkoutSelection" dataKey="nr" :value="props.workouts" showGridlines
+             class="custom-table-Design" resizableColumns columnResizeMode="fit">
+    <Column selectionMode="single" headerStyle="width: 3rem"></Column>
+    <Column field="nr" header="Nr" style="width: 10%"></Column>
+    <Column field="generalInformation.date" header="Date" sortable style="width: 30%"/>
+    <Column field="generalInformation.name" header="Name" sortable style="width: 30%"/>
+    <Column field="suppIntakeInfo.bcaa" header="BCAA" style="width: 5%"/>
+    <Column field="suppIntakeInfo.bcaaAmount" header="gr" style="width: 2%"/>
+    <Column field="suppIntakeInfo.creatin" header="Creatin" style="width: 5%"/>
+    <Column field="suppIntakeInfo.creatinAmount" header="gr" style="width: 2%"/>
+    <Column field="suppIntakeInfo.eaa" header="EAA" style="width: 5%"/>
+    <Column field="suppIntakeInfo.eaaAmount" header="gr" style="width: 2%"/>
+    <Column field="suppIntakeInfo.protein" header="Protein" style="width: 5%"/>
+    <Column field="suppIntakeInfo.proteinAmount" header="gr" style="width: 2%"/>
+  </DataTable>
 </template>
 
 <style scoped>
-.table-toolbar-container {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch; /* Ensures children (toolbar and table) fill the container */
-}
 
-.custom-table-Design {
-  width: 100%
-}
-
-.toolbar-container {
-  margin-bottom: 10px; /* Space between the toolbar and the DataTable */
-}
-
-.toolbar-button:not(:last-child) {
-  margin-right: 10px; /* Adjust the space as needed */
-}
 
 </style>
--- pages/workoutOverview.vue
+++ pages/workoutOverview.vue
@@ -1,46 +1,159 @@
 <script setup lang="ts">
-import {fetchWorkoutData} from "~/firebase/DBController";
+import {deleteWorkoutData, fetchAndLogAllData, fetchWorkoutData} from "~/firebase/DBController";
 import type {WorkoutData} from "~/models/formData/workoutData";
 import {ref} from "vue";
+import Toolbar from "primevue/toolbar";
 
 const workouts = ref<WorkoutData[]>([]);
 const currentWorkoutSelection = ref<WorkoutData | null>(null);
+const isEditMode = ref(false);
+const dataEntryFormVisible = ref(false);
 
 
-//refreshes the DataTAble when new data is added or removed
-async function refreshData() {
+onBeforeMount(async () => {
   const data = await fetchWorkoutData();
+  //console.log('data => ', data)
+  //@ts-ignore
   workouts.value = data.map((workout, index) => ({
     ...workout,
     id: workout.id,
     nr: index + 1
   }));
+  // For Debugging purposes
+  console.log(data);
+});
+
+// For debug purposes
+onMounted(() => {
+  fetchAndLogAllData();
+});
+
+//refreshes the DataTable when new data is added or removed
+async function refreshData() {
+  const data = await fetchWorkoutData();
+  //@ts-ignore
+  workouts.value = data.map((workout, index) => ({
+    ...workout,
+    id: workout.id,
+    nr: index + 1
+  }));
+
 }
 
+// deletes the current selected workout
+async function deleteSelectedWorkout() {
+  if (currentWorkoutSelection.value) {
+    try {
+      //@ts-ignore
+      await deleteWorkoutData(currentWorkoutSelection.value.id);
+      await refreshData();
+      currentWorkoutSelection.value = null;
+      console.log("Workout successfully deleted");
+    } catch (error) {
+      console.error("Failed to delete workout:", error);
+    }
+  } else {
+    console.log("No workout selected for deletion");
+  }
+}
+
+// formates the date to dd/mm/yyy
+function formatDate(date: Date): string {
+  const day = ('0' + date.getDate()).slice(-2);
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
+function toggleDataEntryForm() {
+  isEditMode.value = false;
+  dataEntryFormVisible.value = !dataEntryFormVisible.value;
+}
+
+async function openFormInEditMode() {
+  if (currentWorkoutSelection.value) {
+    try {
+      dataEntryFormVisible.value = true;
+      isEditMode.value = true;
+      await refreshData();
+      currentWorkoutSelection.value = null;
+      console.log("Workout successfully edited");
+    } catch (error) {
+      console.error("Failed to edit workout:", error);
+    }
+  } else {
+    console.log("No workout selected for deletion")
+  }
+}
+
+function updateCurrentSelection(selectedWorkout: WorkoutData) {
+  currentWorkoutSelection.value = selectedWorkout;
+}
+
+// for debugging purposes
+watch(currentWorkoutSelection, (newValue) => {
+  console.log(JSON.stringify(newValue))
+})
 
 </script>
 
 <template>
 
   <div class="page-container">
-    <DataEntryForm/>
-    <WorkoutDataTable/>
+    <DataEntryForm v-if="!isEditMode" v-model:visible="dataEntryFormVisible" :isEditMode="false"
+                   @dataSaved="refreshData" :prefilledWorkoutData="currentWorkoutSelection"/>
+    <DataEntryForm v-else v-model:visible="dataEntryFormVisible" :isEditMode="true"
+                   @dataSaved="refreshData" :prefilledWorkoutData="currentWorkoutSelection"/>
+    <div class="table-toolbar-container">
+      <div class="toolbar-container">
+        <Toolbar>
+          <template #start>
+            <Button class="toolbar-button" label="New" icon="pi pi-plus" @click="toggleDataEntryForm"/>
+            <Button class="toolbar-button" label="Edit" icon="pi pi-file-edit" severity="secondary"
+                    @click="openFormInEditMode"/>
+            <Button class="toolbar-button" label="Delete" icon="pi pi-trash" severity="danger"
+                    @click="deleteSelectedWorkout"/>
+            <Button class="toolbar-button" label="Refresh Table" icon="pi pi-undo" severity="info"
+                    @click="refreshData"/>
+          </template>
+        </Toolbar>
+      </div>
+      <WorkoutDataTable :workouts="workouts" @updateSelection="updateCurrentSelection"/>
+    </div>
   </div>
 </template>
 
 <style scoped>
-/* Ensure the parent container (could be NuxtPage or a specific div around WorkoutDataTable)
-   is styled to fill the available space minus the TopToolbar height and centered */
+
+
 .page-container {
   display: flex;
   justify-content: center; /* Center horizontally */
   align-items: center; /* Center vertically */
-  height: calc(200vh - var(--top-toolbar-height)); /* Adjust height based on the toolbar height */
+  height: calc(2vh - var(--top-toolbar-height)); /* Adjust height based on the toolbar height */
   padding-top: var(--top-toolbar-height); /* If toolbar is fixed at the top */
 }
 
-/* Optional: if you know the exact height of your TopToolbar, replace var(--top-toolbar-height) with that value */
 :root {
   --top-toolbar-height: 60px; /* Example: Adjust this value to match your TopToolbar's height */
 }
+
+.table-toolbar-container {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch; /* Ensures children (toolbar and table) fill the container */
+}
+
+.custom-table-Design {
+  width: 100%
+}
+
+.toolbar-container {
+  margin-bottom: 10px; /* Space between the toolbar and the DataTable */
+}
+
+.toolbar-button:not(:last-child) {
+  margin-right: 10px; /* Adjust the space as needed */
+}
+
 </style>
