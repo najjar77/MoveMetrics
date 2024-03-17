15e4859ce294f844db393ab940fab51b3ee6f871
Amiin Najjar <najjar.amiin@gmail.com>
2024-03-17 17:11:26 +0100

    added menu plus navigation

--- components/TopToolbar.vue
+++ components/TopToolbar.vue
@@ -2,20 +2,76 @@
 import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
 import Avatar from "primevue/avatar";
+
+const visibilityOfMenu = ref(false);
+
+const router = useRouter();
+
+const toggle = (event: Event) => {
+  menu.value.toggle(event);
+};
+const menu = ref();
+const menuItems = ref([{
+  label: 'Dashboard',
+  value: 'dashboard',
+  icon: 'pi pi-home',
+  command: () => {
+    router.push('/');
+  }
+}, {
+  label: 'Data Table',
+  value: 'DataTable',
+  icon: 'pi pi-chart-bar',
+  command: () => {
+    router.push('/workoutOverview');
+  }
+}, {
+  label: 'Statistics',
+  value: 'statistics',
+  icon: 'pi pi-chart-bar',
+  command: () => {
+    router.push('/statistics');
+  }
+}
+]);
+
 </script>
 
 <template>
   <Toolbar style="border-radius: 4rem;">
     <template #start>
-      <img src="../assets/MMLogo2.png" alt="Icon" class="toolbar-icon">
+      <img
+        src="../assets/MMLogo2.png"
+        alt="Icon"
+        class="toolbar-icon"
+      >
       <label class="toolbar-label">MoveMetrics</label>
-      <Button size="medium" icon="pi pi-bars" severity="secondary" class="toolbar-button"/>
+      <Button
+        size="medium"
+        icon="pi pi-bars"
+        severity="secondary"
+        class="toolbar-button"
+        @click="toggle"
+      />
+      <Menu
+        id="overlay_menu"
+        ref="menu"
+        :model="menuItems"
+        :popup="true"
+      />
     </template>
-    <template #center>
-    </template>
+    <template #center />
     <template #end>
-      <Button size="medium" icon="pi pi-wrench" severity="secondary" class="toolbar-button"/>
-      <Avatar image="https://www.gravatar.com/avatar/05dfd4b41340d09cae045235eb0893c3?d=mp" class="toolbar-avatar"/>
+      <Button
+        size="medium"
+        icon="pi pi-wrench"
+        severity="secondary"
+        class="toolbar-button"
+      />
+      <Avatar
+        image="https://www.gravatar.com/avatar/05dfd4b41340d09cae045235eb0893c3?d=mp"
+        class="toolbar-avatar"
+      />
     </template>
   </Toolbar>
 </template>
